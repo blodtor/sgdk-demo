85a8933510901f20d3522415e151f6e5699f178c
BlodTor <blodtor@gmail.com>
2025-05-19 01:10:32 +0300

    Слой WINDOW на слое BG_A

--- res/resouces.h
+++ res/resouces.h
@@ -5,6 +5,7 @@
 
 extern const Image bg_sega;
 extern const Image bg_B;
+extern const Image bg_W;
 extern const TileSet tileset_fg;
 extern const MapDefinition map_fg;
 extern const Palette palette_fg;
